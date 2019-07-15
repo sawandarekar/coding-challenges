e73e4f48d992ef3b017d73da3ef27676fbe19450
TTG\darekar <sawan.darekar@tomtom.com>
2019-07-15 14:12:32 +0530

    Day14 scope

---
+++ src/main/java/org/techHub/hackerRank/thirtyDaysOfCode/Day14Scope.java
@@ -0,0 +1,37 @@
+package org.techHub.hackerRank.thirtyDaysOfCode;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Day14Scope {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        sc.close();
+
+        Difference difference = new Difference(a);
+
+        difference.computeDifference();
+
+        System.out.print(difference.maximumDifference);
+    }
+}
+class Difference {
+    private int[] elements;
+    public int maximumDifference;
+
+    Difference(int[] e){
+        this.elements = e;
+    }
+
+    public void computeDifference(){
+        Arrays.sort(elements);
+        maximumDifference = elements[elements.length-1]-elements[0];
+    }
+
+}
