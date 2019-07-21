77e9fb362593308b6ce231b42c2d5c304c53c05c
TTG\darekar <sawan.darekar@tomtom.com>
2019-07-21 12:10:29 +0530

    Day22 BinaryTreeSearches

---
+++ src/main/java/org/techHub/hackerRank/thirtyDaysOfCode/Day22BinaryTreeSearches.java
@@ -0,0 +1,60 @@
+package org.techHub.hackerRank.thirtyDaysOfCode;
+
+import java.util.Scanner;
+
+/**
+ * https://www.hackerrank.com/challenges/30-binary-search-trees
+ */
+public class Day22BinaryTreeSearches {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int T=sc.nextInt();
+        BinaryTreeNode root=null;
+        while(T-->0){
+            int data=sc.nextInt();
+            root=insert(root,data);
+        }
+        int height=getHeight(root);
+        System.out.println(height);
+    }
+    public static BinaryTreeNode insert(BinaryTreeNode node, int data){
+        if(node == null){
+            return new BinaryTreeNode(data);
+        }else{
+            BinaryTreeNode curr;
+            if(data < node.data){
+                curr = new BinaryTreeNode(data);
+                node.left = curr;
+            }else{
+                curr = new BinaryTreeNode(data);
+                node.right = curr;
+            }
+            return  node;
+        }
+    }
+    public static int getHeight(BinaryTreeNode root){
+//        if(root == null){
+//            return  -1;
+//        }
+//        int leftDepth = getHeight(root.left);
+//        int rightDepth = getHeight(root.right);
+//        return (leftDepth > rightDepth ? leftDepth : rightDepth) + 1;
+//
+        if(root == null) {
+            return -1;
+        }
+        int left = 1 + getHeight(root.left);
+        int right = 1 + getHeight(root.right);
+
+        return Math.max(left, right);
+    }
+}
+class BinaryTreeNode{
+    BinaryTreeNode left,right;
+    int data;
+    BinaryTreeNode(int d){
+        this.data = d;
+        left=right=null;
+    }
+}
