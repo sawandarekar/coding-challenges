d13b347417036e22ad900bcc98911bb34df2ceb9
TTG\darekar <sawan.darekar@tomtom.com>
2019-07-21 23:55:48 +0530

    Day 25 Running time and complexity

--- src/main/java/org/techHub/hackerRank/thirtyDaysOfCode/Day25_RunningTimeAndComplexity.java
+++ src/main/java/org/techHub/hackerRank/thirtyDaysOfCode/Day25_RunningTimeAndComplexity.java
@@ -2,6 +2,7 @@ package org.techHub.hackerRank.thirtyDaysOfCode;
 
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 /**
  *
@@ -15,6 +16,31 @@ public class Day25_RunningTimeAndComplexity {
         for (int i = 0; i < numbers; i++) {
             n[i] = sc.nextInt();
         }
-        Arrays.stream(n).forEach(System.out::println);
+        //Arrays.stream(n).forEach(System.out::println);
+        for (int i = 0; i < n.length; i++) {
+            int numb = n[i];
+            boolean isPrime = true;
+            for (int j = 2; j < numb; j++) {
+                if(numb % j == 0){
+                    isPrime = false;
+                    System.out.println("Not prime");
+                    break;
+                }
+            }
+            if(isPrime){
+                System.out.println("Prime");
+            }
+        }
+        System.out.println("----------------------");
+        for (int i = 0; i < n.length; i++) {
+            int val = n[i];
+            for(int j=2; j<=val/j; j++){
+                if(val%j==0){
+                    val=1;
+                    break;
+                }
+            }
+            System.out.println(val==1?"Not prime":"Prime");
+        }
     }
 }
